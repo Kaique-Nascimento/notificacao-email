8282fe3ff4d23c44993737b17aa1e8bd00acfa83
Kaique Nascimento <kaiquepalmanascimento@hotmail.com>
2023-02-02 13:52:28 -0300

    deixado um pouco mais bonito
    ta um pouco mais bonito do que o anterior

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ var texto = document.querySelector('#texto');
 var botao = document.querySelector('#botao');
 var assunto = document.querySelector('#assunto');
 var email = document.querySelector('#email');
+var email = document.querySelector('#mensagem');
 
 botao.addEventListener('click', function(){
     try{
         if(!email.value && !assunto.value && !texto.value) alert("Digite");
         else{
             fetch(`https://ntfy.sh/mensagem`, {
-                method: 'POST',
+                method: 'POST', // PUT works too
                 headers: {
                     'Title' : `${assunto.value}`,
                     'Email': `${email.value}`
                 },
                 body: `Eu só queria dizer o seguinte: ${texto.value}`,
             });
-            alert("Email enviado!");
-        }
+            alert("Email enviado!");}
         }
         
     catch (e){
         alert ("Algo deu errado: "+e);
     }
-});
+});  
+document.addEventListener("DOMContentLoaded", function(event) {
+    if(cont >= 1) mensagem.style.display = "block";
+    console.log(cont);
+  });
